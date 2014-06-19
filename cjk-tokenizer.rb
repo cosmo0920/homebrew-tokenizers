4363bf229b92110124a2320da0598f7a25b237a0
hhatake <hhatake@aiming-inc.com>
2014-06-19 10:30:30 +0900

    use '(single quote)

--- cjk-tokenizer.rb
+++ cjk-tokenizer.rb
@@ -1,11 +1,11 @@
-require "formula"
+require 'formula'
 
 class CjkTokenizer < Formula
-  homepage "https://github.com/rboulton/cjk-tokenizer"
-  url "https://github.com/rboulton/cjk-tokenizer/archive/eb7ec18aabcbc9368542f64dab0ed8f424d52290.zip"
-  sha256 "df1f1e635705f8dcb468c4c4f3e60f1be6b4464afda3688b1a587f781fcea0ed"
+  homepage 'https://github.com/rboulton/cjk-tokenizer'
+  url 'https://github.com/rboulton/cjk-tokenizer/archive/eb7ec18aabcbc9368542f64dab0ed8f424d52290.zip'
+  sha256 'df1f1e635705f8dcb468c4c4f3e60f1be6b4464afda3688b1a587f781fcea0ed'
 
-  depends_on "xapian"
+  depends_on 'xapian'
 
   def install
     inreplace 'cjk-tokenizer/Makefile' do |s|
@@ -15,8 +15,8 @@ class CjkTokenizer < Formula
       s.gsub! '/usr/local', prefix
     end
     system 'cd cjk-tokenizer && make'
-    lib.install "cjk-tokenizer/libcjk-tokenizer.a"
-    include.install "cjk-tokenizer/cjk-hanconvert.h"
-    include.install "cjk-tokenizer/cjk-tokenizer.h"
+    lib.install 'jk-tokenizer/libcjk-tokenizer.a'
+    include.install 'cjk-tokenizer/cjk-hanconvert.h'
+    include.install 'cjk-tokenizer/cjk-tokenizer.h'
   end
 end
