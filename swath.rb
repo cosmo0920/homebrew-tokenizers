077449a52f77462039934b6e548abc3383812f28
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-25 00:14:00 +0900

    add `swath` formula
    SWATH (Smart Word Analysis for THai)

---
+++ swath.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Swath < Formula
+  homepage 'http://www.cs.cmu.edu/~paisarn/software.html'
+  url 'ftp://linux.thai.net/pub/thailinux/software/swath/swath-0.5.2.tar.xz'
+  sha256 '233ef8effa5d21bfe4798b6f415ccbdd4460bf02e9e6cbb769d7585d6cf2fa1d'
+  head 'http://linux.thai.net/svn/software/swath/trunk', :using => :svn
+
+  depends_on 'pkg-config' => :build
+  depends_on 'libtool'
+  depends_on 'cosmo0920/tokenizers/datrie'
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    system './configure', *args
+    system 'make install'
+  end
+end
