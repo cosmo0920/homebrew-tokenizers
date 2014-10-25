190632f1890062171a923ab037e804b1bdb6b305
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-10-25 23:23:46 +0900

    add libstemmer_c formula

---
+++ libstemmer.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Libstemmer < Formula
+  homepage "http://snowball.tartarus.org"
+  url "http://snowball.tartarus.org/dist/libstemmer_c.tgz"
+  sha1 "9b0f120a68a3c688b2f5a8d0f681620465c29d38"
+
+  def install
+    # Hardcorded include path :<
+    build_dir = Dir.pwd
+    inreplace "Makefile" do |s|
+      s.gsub! "-Iinclude", "-I #{build_dir}/include"
+    end
+    shard_args = %Q(-O2 -Wall -fPIC -shared)
+
+    system "make"
+    system "#{ENV.cc} #{shard_args} #{build_dir}/libstemmer.o -o #{build_dir}/libstemmer.dylib"
+    include.install "include"
+    bin.install "stemwords"
+    lib.install "libstemmer.dylib"
+  end
+end
