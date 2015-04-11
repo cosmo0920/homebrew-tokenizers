15885d9ad4c1e4602c333f720fa1777c2bf76f06
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-04-11 18:52:14 +0900

    Modernize (Use double quotations)

--- datrie.rb
+++ datrie.rb
@@ -1,8 +1,8 @@
 class Datrie < Formula
-  homepage 'http://linux.thai.net/projects/datrie'
-  url 'ftp://linux.thai.net/pub/ThaiLinux/software/libthai/libdatrie-0.2.4.tar.gz'
-  sha256 'b2acfbfd9df07780f91352bc1bb16e4d8a32772dc9194bbc35ae600e0591dfa2'
-  head 'http://linux.thai.net/svn/software/datrie', :using => :svn
+  homepage "http://linux.thai.net/projects/datrie"
+  url "ftp://linux.thai.net/pub/ThaiLinux/software/libthai/libdatrie-0.2.4.tar.gz"
+  sha256 "b2acfbfd9df07780f91352bc1bb16e4d8a32772dc9194bbc35ae600e0591dfa2"
+  head "http://linux.thai.net/svn/software/datrie", :using => :svn
 
   def install
     args = %W[
@@ -10,7 +10,7 @@ class Datrie < Formula
       --prefix=#{prefix}
     ]
 
-    system './configure', *args
-    system 'make install'
+    system "./configure", *args
+    system "make install"
   end
 end
--- swath.rb
+++ swath.rb
@@ -1,12 +1,12 @@
 class Swath < Formula
-  homepage 'http://www.cs.cmu.edu/~paisarn/software.html'
-  url 'ftp://linux.thai.net/pub/thailinux/software/swath/swath-0.5.2.tar.xz'
-  sha256 '233ef8effa5d21bfe4798b6f415ccbdd4460bf02e9e6cbb769d7585d6cf2fa1d'
-  head 'http://linux.thai.net/svn/software/swath/trunk', :using => :svn
+  homepage "http://www.cs.cmu.edu/~paisarn/software.html"
+  url "ftp://linux.thai.net/pub/thailinux/software/swath/swath-0.5.2.tar.xz"
+  sha256 "233ef8effa5d21bfe4798b6f415ccbdd4460bf02e9e6cbb769d7585d6cf2fa1d"
+  head "http://linux.thai.net/svn/software/swath/trunk", :using => :svn
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libtool'
-  depends_on 'cosmo0920/tokenizers/datrie'
+  depends_on "pkg-config" => :build
+  depends_on "libtool"
+  depends_on "cosmo0920/tokenizers/datrie"
 
   def install
     args = %W[
@@ -14,8 +14,8 @@ class Swath < Formula
       --prefix=#{prefix}
     ]
 
-    system './autogen.sh' if build.head?
-    system './configure', *args
-    system 'make install'
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make install"
   end
 end
