c7c72324cd060c4386b556938987b000c0c48b42
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-04-11 19:08:00 +0900

    Use trunk branch

--- datrie.rb
+++ datrie.rb
@@ -2,7 +2,7 @@ class Datrie < Formula
   homepage "http://linux.thai.net/projects/datrie"
   url "ftp://linux.thai.net/pub/ThaiLinux/software/libthai/libdatrie-0.2.4.tar.gz"
   sha256 "b2acfbfd9df07780f91352bc1bb16e4d8a32772dc9194bbc35ae600e0591dfa2"
-  head "http://linux.thai.net/svn/software/datrie", :using => :svn
+  head "http://linux.thai.net/svn/software/datrie/trunk", :using => :svn
 
   def install
     args = %W[
