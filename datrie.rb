c58271a73b26ed80d2b77d5236be63a8261a8348
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-04-11 19:08:12 +0900

    Really support trunk build

--- datrie.rb
+++ datrie.rb
@@ -4,12 +4,17 @@ class Datrie < Formula
   sha256 "b2acfbfd9df07780f91352bc1bb16e4d8a32772dc9194bbc35ae600e0591dfa2"
   head "http://linux.thai.net/svn/software/datrie/trunk", :using => :svn
 
+  depends_on :automake => :build if build.head?
+  depends_on :autoconf => :build if build.head?
+  depends_on :libtool  => :build if build.head?
+
   def install
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
 
+    system "./autogen.sh" if build.head?
     system "./configure", *args
     system "make install"
   end
