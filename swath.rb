d7259454a1fcdad2b44e9a6202702190706b3c16
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-04-17 01:36:22 +0900

    swath: 0.5.3

--- swath.rb
+++ swath.rb
@@ -1,7 +1,7 @@
 class Swath < Formula
   homepage "http://www.cs.cmu.edu/~paisarn/software.html"
-  url "ftp://linux.thai.net/pub/thailinux/software/swath/swath-0.5.2.tar.xz"
-  sha256 "233ef8effa5d21bfe4798b6f415ccbdd4460bf02e9e6cbb769d7585d6cf2fa1d"
+  url "ftp://linux.thai.net/pub/thailinux/software/swath/swath-0.5.3.tar.xz"
+  sha256 "24d4c7b4184c0ffde10af5b7e36ccba82f3cf7e7b7845af51b53eb0b2397a136"
   head "http://linux.thai.net/svn/software/swath/trunk", :using => :svn
 
   depends_on "pkg-config" => :build
